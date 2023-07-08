ebca0017ed745d3cc3beb3ee5dd87b57ddd7a2e2
juanvictorf <estudantejuan0@gmail.com>
2023-07-08 10:43:01 -0300

    Subindo projeto alterado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./style.css";
+import React, { useState} from "react";
+import axios from "axios";
+
+
 
 function App() {
+
+const [cep, setCep] = useState('');
+const [endereco, setEndereco] = useState(null);
+
+const handleCepChange = (event) => {
+  setCep(event.target.value);
+};
+
+const buscarEndereco = () => {
+  axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => {
+      setEndereco(response.data);
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      setEndereco(null);
+    });
+};
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>API Busca CEP</h1>
+        <input type="text" value={cep} onChange={handleCepChange} />
+        <button onClick={buscarEndereco} className="botao">Buscar CEP</button>
+
+      
+      {endereco && (
+          <div className="resultado">
+            <p>CEP: {endereco.cep}</p>
+            <p>Logradouro: {endereco.logradouro}</p>
+            <p>Bairro: {endereco.bairro}</p>
+            <p>Cidade: {endereco.localidade}</p>
+            <p>Estado: {endereco.uf}</p>
+          </div>
+        )}
+      
+      </div>
   );
 }
+
 
 export default App;
